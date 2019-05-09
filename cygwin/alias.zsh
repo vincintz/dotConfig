79aba0840b526d48018c92ef75ca5c43a5db15fd
Vincintz Theo Reyes <vincintz.reyes@datalex.com>
2019-05-09 04:00:18 +0100

    Updated options

--- cygwin/alias.zsh
+++ cygwin/alias.zsh
@@ -2,3 +2,4 @@ alias ifconfig=ipconfig
 alias edit="/c/Program\ Files/Notepad++/notepad++.exe"
 alias open="cygstart"
 alias sudo="python3 /usr/local/lib/cygwin-sudo/cygwin-sudo.py"
+alias ps="ps -W"
