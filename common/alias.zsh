47641693973b5b20ff704e3fc851b829f0a967a3
Vincintz Theo Reyes <vincintz.reyes@datalex.com>
2019-10-17 06:16:21 +0100

    Add diff-so-fancy in alias

--- common/alias.zsh
+++ common/alias.zsh
@@ -1,6 +1,8 @@
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo'
+alias diff='/opt/tools/diff-so-fancy'
 alias gts='git status'
 alias gtcx='git add . && git commit -am'
+alias gtd='git diff | diff'
 alias myip="curl http://ipecho.net/plain; echo"
