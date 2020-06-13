5323cefac4c50e24d9a4d279e55d313b634b5f67
Theo Reyes <vincintz@yahoo.com>
2020-06-13 19:25:59 +0800

    Fixed diff issue

--- common/alias.zsh
+++ common/alias.zsh
@@ -1,7 +1,6 @@
 alias ls='ls -G'
 alias grep='grep --color=auto'
 alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo'
-alias diff='/opt/tools/diff-so-fancy'
 alias gts='git status'
 alias gtcx='git add . && git commit -am'
 alias gtd='git diff | diff'
--- cygwin/alias.zsh
+++ cygwin/alias.zsh
@@ -4,3 +4,4 @@ alias open="cygstart"
 alias sudo="python3 /usr/local/lib/cygwin-sudo/cygwin-sudo.py"
 alias ps="ps -W"
 alias ping=PING
+alias diff='/opt/tools/diff-so-fancy'
