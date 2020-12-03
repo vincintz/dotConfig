ac9031aecd6023be5fa99e0a4f3d7e90bb0c5ad3
Theo Reyes <theo.reyes@simplyrfid.com>
2020-12-04 02:42:10 +0800

    Changed git status prompt color

--- common/prompt.zsh
+++ common/prompt.zsh
@@ -4,7 +4,7 @@ setopt prompt_subst
 usr_prompt() {
     id -G | grep -qE '\<(114|544)\>' &> /dev/null
     if [ $? -eq 0 ]; then
-        echo '%F{magenta}#%f'
+        echo '%F{red}#%f'
     else
         echo '%#'
     fi
@@ -13,7 +13,7 @@ git_info() {
     local gitbranch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
     if [ -n "$gitbranch" ]; then
         if [ -n "$(git status --porcelain 2> /dev/null)" ]; then
-            echo "%F{magenta}($gitbranch)%f "
+            echo "%F{red}($gitbranch)%f "
         else
             echo "%F{cyan}($gitbranch)%f "
         fi
