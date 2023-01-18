651342f717d8a4c6610ff18f97481c77aab05f1a
Theo Reyes <vincintz@yahoo.com>
2023-01-18 17:13:36 +0800

    Useful alias

--- common/alias.zsh
+++ common/alias.zsh
@@ -2,3 +2,4 @@ alias ls='ls --color --group-directories-first'
 alias grep='grep --color=auto'
 alias myip="curl https://ifconfig.me; echo"
 alias tree="tree -C | less -r"
+alias ncdu="ncdu --color=dark"
