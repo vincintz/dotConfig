1a99c418218f228a6f0a987bea646497e3b952be
Theo Reyes <vincintz@yahoo.com>
2023-01-31 16:17:50 +0800

    alias for mysql with pager

--- common/alias.zsh
+++ common/alias.zsh
@@ -3,4 +3,4 @@ alias grep='grep --color=auto'
 alias myip="curl https://ifconfig.me; echo"
 alias tree="tree -C | less -r"
 alias ncdu="ncdu --color=dark"
-alias mysql="mysql --pager='pspg -X --quit-if-one-screen' --safe-updates --select-limit=500"
+alias mysql="mysql --pager='pspg -X -F' --safe-updates --select-limit=500"
