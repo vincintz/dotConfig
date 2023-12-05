e02597a507233b4f25cfd1de9c2ce6d4e752b50a
Theo Reyes <vincintz@yahoo.com>
2023-12-05 14:05:47 +0800

    Telescope file browser

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -12,12 +12,22 @@ return {
       local builtin = require("telescope.builtin")
       telescope.load_extension("fzf")
       -- keymaps
-      vim.keymap.set('n', 'ff', builtin.find_files, {})
-      vim.keymap.set('n', 'fg', builtin.git_files, {})
-      vim.keymap.set('n', 'fs', builtin.live_grep, {})
-      vim.keymap.set('n', 'fb', builtin.buffers, {})
-      vim.keymap.set('n', 'fh', builtin.help_tags, {})
-      vim.keymap.set('n', 'fc', builtin.grep_string, {})
+      vim.keymap.set("n", "fg", builtin.git_files, {})
+      vim.keymap.set("n", "fs", builtin.live_grep, {})
+      vim.keymap.set("n", "fb", builtin.buffers, {})
+      vim.keymap.set("n", "fh", builtin.help_tags, {})
+      vim.keymap.set("n", "fc", builtin.grep_string, {})
     end,
   },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      vim.keymap.set("n", "ff", "<cmd>Telescope file_browser<cr>", {})
+    end,
+  }
 }
