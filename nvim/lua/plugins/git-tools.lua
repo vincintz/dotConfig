c9e8895fc5c4dec2aed41a32c2d602c7853c3a47
Theo Reyes <vincintz@yahoo.com>
2023-12-23 23:50:02 +0800

    working lsp

--- nvim/lua/plugins/git-tools.lua
+++ nvim/lua/plugins/git-tools.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       vim.keymap.set('n', 'gs', "<cmd>Git<cr><cmd>resize 10<cr>")
       vim.keymap.set('n', 'gb', "<cmd>Git blame<cr>")
-      vim.keymap.set('n', 'gd', "<cmd>Gdiffsplit<cr><c-w><c-w>")
+      -- vim.keymap.set('n', 'gd', "<cmd>Gdiffsplit<cr><c-w><c-w>")
     end
   },
   "mhinz/vim-signify",
---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "pyright",
+          "lua_ls",
+          "tsserver",
+        },
+        automatic_installation = true,
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({})
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = {}
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        end,
+      })
+    end
+  },
+}
