eb6ba48083f9f48f7b581d82577fc9d7dd63cb41
Theo Reyes <vincintz@yahoo.com>
2023-12-28 14:02:58 +0800

    nvim: use none-ls

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,13 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "python", "javascript", "regex", "json", "html" },
-      })
-    end
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
 }
