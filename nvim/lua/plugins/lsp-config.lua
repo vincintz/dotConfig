de35bdc675fa271315f1c749412dbdc1e305813b
Theo Reyes <vincintz@yahoo.com>
2024-02-07 00:43:49 +0800

    nvim change git key bindings

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -52,8 +52,8 @@ return {
                 },
             })
             vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>gD", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gR", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
             local sign = function(opts)
