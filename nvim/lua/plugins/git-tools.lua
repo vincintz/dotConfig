bf0535267e1650d03fd433949ecaf90bb782e0d6
Theo Reyes <vincintz@yahoo.com>
2024-02-07 23:40:21 +0800

    nvim fugitive updates

--- nvim/lua/plugins/git-tools.lua
+++ nvim/lua/plugins/git-tools.lua
@@ -1,11 +1,12 @@
 return {
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set('n', 'gs', "<cmd>Git<cr><cmd>resize 10<cr>")
-      vim.keymap.set('n', 'gb', "<cmd>Git blame<cr>")
-      vim.keymap.set('n', 'gd', "<cmd>Gdiffsplit<cr><c-w><c-w>")
-    end
-  },
-  "mhinz/vim-signify",
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.opt.diffopt:append {"iwhiteall", "vertical"}
+            vim.keymap.set('n', 'gs', "<cmd>Git<cr><cmd>resize 10<cr>")
+            vim.keymap.set('n', 'gb', "<cmd>Git blame<cr>")
+            vim.keymap.set('n', 'gd', "<cmd>Gdiffsplit<cr><c-w><c-w>")
+        end
+    },
+    "mhinz/vim-signify",
 }
