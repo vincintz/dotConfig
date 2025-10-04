eff2fe005bb86099830c8d13a022c901aa1f5113
Theo Reyes <vincintz@yahoo.com>
2025-10-04 23:09:02 +0800

    nvim update lspconfig

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- vim.lsp.enable("pyright")
+      vim.lsp.enable("basedpyright")
+      vim.lsp.enable("luarocks")
+    end
+  },
+  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup({
@@ -21,84 +29,10 @@ return {
           "lua_ls",
           "ts_ls",
           "html",
+          "basedpyright",
         },
         automatic_installation = true,
       })
     end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-      local lspconfig = require("lspconfig")
-      local util = require 'lspconfig.util'
-      lspconfig.pyright.setup({
-        capabilites = capabilities,
-        settings = {
-          python = {
-            analysis = {
-              autoImportCompletions = "off",
-              typeCheckingMode = "off",
-              diagnosticMode = "openFilesOnly",
-            },
-          },
-        },
-      })
-      lspconfig.ts_ls.setup({
-        capabilites = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilites = capabilities,
-      })
-      lspconfig.eslint.setup({
-        root_dir = util.root_pattern(
-          ".eslintrc",
-          ".eslintrc.js",
-          ".eslintrc.cjs",
-          ".eslintrc.yaml",
-          ".eslintrc.yml",
-          ".eslintrc.json"
-        ),
-      })
-      lspconfig.emmet_language_server.setup({
-        filetypes = { "css", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" }
-      })
-      lspconfig.lua_ls.setup({
-        capabilites = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-
-      vim.diagnostic.config({
-        virtual_text = false,
-        severity_sort = true,
-        float = {
-          border = 'rounded',
-          source = 'always',
-        },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = "✘",
-            [vim.diagnostic.severity.WARN] = "▲",
-            [vim.diagnostic.severity.INFO] = "»",
-            [vim.diagnostic.severity.HINT] = "⚑",
-          },
-          linehl = {
-            [vim.diagnostic.severity.ERROR] = "Error",
-            [vim.diagnostic.severity.WARN] = "Warn",
-            [vim.diagnostic.severity.INFO] = "Info",
-            [vim.diagnostic.severity.HINT] = "Hint",
-          },
-        },
-      })
-    end,
   },
 }
